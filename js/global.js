8cb5c7174c853e73fa97f354e0db176ae483e98f
lidekai <960023381@qq.com>
2019-04-12 17:11:42 +0800

    修改细节样式

--- js/global.js
+++ js/global.js
@@ -1,5 +1,71 @@
 var globalUrl = 'http://39.98.33.48/';
 $('.mavList li').click(function() {
 	var tarUrl = $(this).attr("data-url");
-	window.location.href= globalUrl + tarUrl;
+	window.location.href = globalUrl + tarUrl;
+})
+
+//顶部导航栏鼠标移入事件
+$('.mavList li').mouseover(function() {
+	if ($(this).hasClass('active') == false) {
+		//添加底部边框显示的动画
+		$(this).find('.line').animate({
+			width: '100%',
+			borderBottomWidth: '6px'
+		}, 100)
+		//添加导航内容的动画并添加颜色
+		$(this).find('span.txt').animate({
+			top: '0px',
+		}, {
+			duration: 100,
+			complete: function() {
+				$(this).css("color", "#3ba8cf");
+			}
+		})
+	}
+})
+$('.mavList li').mouseleave(function() {
+	if ($(this).hasClass('active') == false) {
+		$(this).find('.line').animate({
+			width: '0',
+			borderBottomWidth: '6px'
+		}, 100)
+
+		$(this).find('span.txt').animate({
+			top: '0',
+		}, {
+			duration: 100,
+			complete: function() {
+				$(this).css("color", "#fff");
+			}
+		})
+	}
+})
+//鼠标移入微信显示二维码
+$('li.wx').mouseover(function() {
+	$(this).find('.wxewm').show();
+})
+$('li.wx').mouseleave(function() {
+	$(this).find('.wxewm').hide();
+})
+
+
+//鼠标移入微信显示二维码
+$('li.tel').mouseover(function() {
+	$(this).find('.telBox').show();
+})
+$('li.tel').mouseleave(function() {
+	$(this).find('.telBox').hide();
+})
+
+//鼠标移入微信显示二维码
+$('li.email').mouseover(function() {
+	$(this).find('.emailBox').show();
+})
+$('li.email').mouseleave(function() {
+	$(this).find('.emailBox').hide();
+})
+
+$(".goToLink").click(function() {
+	var tarUrl = $(this).attr("data-url");
+	window.location.href = globalUrl + tarUrl;
 })
